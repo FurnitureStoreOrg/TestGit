b77a2a6987a6452982ac66f53612dcb0272f954e
wangji3721 <jian.wang3721@gmail.com>
2014-09-22 18:33:18 -0400

    Initial checkin of TestGitHub

---
+++ TestGitHub/src/com/github/test/GithubTester.java
@@ -0,0 +1,16 @@
+package com.github.test;
+
+public class GithubTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.err.println("This java project is for testing the checkin/checkout of Github repository.");
+		
+		System.err.println("Test Local Github repository.");
+	}
+
+}
