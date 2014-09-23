d352ea2c40b35d5e2c769bfb599072d017660884
Other Developer <junk.coder.email@gmail.com>
2014-09-22 20:42:05 -0400

    Summed up values when displaying and added class comment.

--- TestGitHub/src/com/github/test/TestData.java
+++ TestGitHub/src/com/github/test/TestData.java
@@ -1,13 +1,20 @@
 package com.github.test;
 
+/**
+ * Represent the sales data for a given period.
+ *
+ */
 public class TestData {
 
 	int data[] = {0 ,4, 42};
 	
 	public void display() {
 		System.out.println("Data: ");
+		int sum = 0;
 		for(int i=0; i < data.length; i++) {
 			System.out.println("Next value: " + data[i]);
+			sum += i;
 		}
+		System.out.println("Sum is: " + sum);
 	}
 }
